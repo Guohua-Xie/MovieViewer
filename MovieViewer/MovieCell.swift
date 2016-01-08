0a11bbada2d22e5fcf234bab944d71817812feeb
MacbookPro <macbookpro@macbookpros-mbp.gateway.pace.com>
2016-01-08 00:05:32 -0800

    First commit

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by MacbookPro on 1/7/16.
+//  Copyright © 2016 MacbookPro. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overViewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
